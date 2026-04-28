47e258e7264c3ff49e8cb28fd31bac7783bbb3d0
eldablo23307 <dany.mottu@gmail.com>
2026-04-28 15:49:05 +0200

    Added frontend and backend

--- Backend/main.py
+++ Backend/main.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, request 
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello world"
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        return "Code: 100"
+    else:
+        return "Code: 400"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
