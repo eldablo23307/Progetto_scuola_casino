b7be7840e9bc96e8fbd41cdc7d51ecbc622e51b7
eldablo23307 <dany.mottu@gmail.com>
2026-04-29 11:04:06 +0200

    Started Login page for frontend

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,16 +1,59 @@
 import 'package:flutter/material.dart';
 
 void main() {
-	runApp(
-		MaterialApp(
-			home: Scaffold(
-				appBar: AppBar(
-					title: const Text("First App")
-					,),
-				body: const Center(
-					child: Text("Hello World"),
-					),
-				)
-			,),
-	);
+    runApp(const Login());
 }
+
+class Login extends StatelessWidget {
+    const Login({super.key});
+    @override
+    Widget build(BuildContext context ) {
+        return MaterialApp(	
+			home: Scaffold(
+				backgroundColor: Color(0xFF593F62),	
+				body: const Center(
+						child: Column(
+							children: [
+								const Text("Brawls Bets", 
+								    style: TextStyle(fontSize: 40, height: 10, color: Color(0xFFA5C4D4), fontFamily: "Roboto")),
+                                const LoginForm()
+							],
+						),
+					),
+				),
+			
+		);
+    }
+}
+
+class LoginForm extends StatelessWidget {
+    const LoginForm({super.key});
+    @override
+    Widget build(BuildContext context){
+        return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+                    const Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20, vertical:  10),
+                        child: const TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                hintText: "Email",
+                                hintStyle: TextStyle(color: Color(0xFFA5C4D4)) 
+                            ),
+                        ),
+                    ),
+                const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                    child: const TextField(
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            hintText: "password",
+                            hintStyle: TextStyle(color: Color(0XFFA5C4D4))
+                        ),
+                    ),
+                )
+            ],
+        );
+    }
+}:w
